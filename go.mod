586bcf2fe932ad8829bd11367b24123b4bc8f9ce
Paul Calzado <pafcalzado@gmail.com>
2024-01-07 19:09:43 +0800

    Add jwt token for authentication

--- go.mod
+++ go.mod
@@ -4,4 +4,8 @@ go 1.21.5
 
 require github.com/go-chi/chi/v5 v5.0.11
 
-require golang.org/x/crypto v0.17.0
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.0
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.17.0
+)
--- main.go
+++ main.go
@@ -4,19 +4,29 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 	"github.com/skullzado/chirpy-project/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits int
 	DB             *database.DB
+	jwtSecret      string
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+
+	godotenv.Load(".env")
+
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		log.Fatal("JWT_SECRET environment variable is not set")
+	}
 
 	db, err := database.NewDB("database.json")
 	if err != nil {
@@ -35,6 +45,7 @@ func main() {
 	apiCfg := apiConfig{
 		fileserverHits: 0,
 		DB:             db,
+		jwtSecret:      jwtSecret,
 	}
 
 	router := chi.NewRouter()
@@ -47,7 +58,9 @@ func main() {
 	apiRouter.Get("/reset", apiCfg.handlerReset)
 
 	apiRouter.Post("/login", apiCfg.handlerLogin)
+
 	apiRouter.Post("/users", apiCfg.handlerUsersCreate)
+	apiRouter.Put("/users", apiCfg.handlerUsersUpdate)
 
 	apiRouter.Post("/chirps", apiCfg.handlerChirpsCreate)
 	apiRouter.Get("/chirps", apiCfg.handlerChirpsRetrieve)
