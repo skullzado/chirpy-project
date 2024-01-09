f95d446f49ccd5e866e591906da8c3a90cf640c3
Paul Calzado <pafcalzado@gmail.com>
2024-01-09 21:11:36 +0800

    Update handler_chirps_get.go to include sort order

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -46,6 +46,12 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
 		}
 	}
 
+	sortDirection := "asc"
+	sortDirectionParam := r.URL.Query().Get("sort")
+	if sortDirectionParam == "desc" {
+		sortDirection = "desc"
+	}
+
 	chirps := []Chirp{}
 	for _, dbChirp := range dbChirps {
 		if authorID != -1 && dbChirp.AuthorID != authorID {
@@ -60,6 +66,9 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
 	}
 
 	sort.Slice(chirps, func(i, j int) bool {
+		if sortDirection == "desc" {
+			return chirps[i].ID > chirps[j].ID
+		}
 		return chirps[i].ID < chirps[j].ID
 	})
 
